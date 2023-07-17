e80b25509d9842d9b9906589134d163c03e777b5
demmy-tee <danielayo654@gmail.com>
2023-07-17 00:46:09 -0700

    alx

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This for printf
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
