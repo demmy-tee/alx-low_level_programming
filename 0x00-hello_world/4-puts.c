894a5ca2e59db97dcbf23a39181b41ae0d318077
demmy-tee <danielayo654@gmail.com>
2025-04-15 14:10:25 +0100

    betty-style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - program that print out a simple statement using puts
+ *
+ * Return: (0) - Success
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle);
 return (0);
 }
