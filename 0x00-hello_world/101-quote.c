c0b7dc2ee81473226f96a0e17524c28121bf3611
demmy-tee <danielayo654@gmail.com>
2025-04-21 17:29:24 +0100

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
 * main - entry point
 *
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
 
