78caa7b64d4b169c64e7d61bdf024765bce168ff
demmy-tee <danielayo654@gmail.com>
2025-05-21 07:49:11 +0100

    upper case and lower case

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
+/**
+ * main - entry point of the program
+ *
+ * Return: always (0) sucess
+ */
+int main(void)
+{
+	for (char ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	}
+	for (char ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
 
